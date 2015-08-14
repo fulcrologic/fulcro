76a69d5338445bbca836af2118544a6534afc2a2
Tony Kay <twkay@thenavisway.com>
2015-08-14 12:40:40 -0700

    re-added dep in proj file for todos

--- examples/todo/project.clj
+++ examples/todo/project.clj
@@ -8,7 +8,7 @@
                  [org.clojure/clojurescript "0.0-3297"]
                  [com.lucasbradstreet/cljs-uuid-utils "1.0.2"]
                  [quiescent "0.2.0-RC1"]
-                 ;[quiescent-model "0.1.0-SNAPSHOT"]
+                 [quiescent-model "0.1.0-SNAPSHOT"]
                  ]
 
   :plugins [[lein-cljsbuild "1.0.5"]
