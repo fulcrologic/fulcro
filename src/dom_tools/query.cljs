4c9637115cce360a42040b5938249a19bcfde55f
Dave Owens <dwowens@thenavisway.com>
2015-08-31 12:19:12 -0700

    Story ID: RPC-174 - finished find-element, with tests

--- src/dom_tools/query.cljs
+++ src/dom_tools/query.cljs
@@ -7,24 +7,49 @@
 
 (defn is-button? [dom-node] (= "button" (str/lower-case (.-nodeName dom-node))))
 
-(defn text-matches [regex dom-node] (.test regex (gd/getTextContent dom-node)))
+(defn text-matches [string dom-node]
+  (let [regex (js/RegExp. string)
+        text (gd/getTextContent dom-node)]
+    (.test regex text)))
 
-(defn is-button-with-label? [string dom-node] (let [regex (js/RegExp. string)]
-                                                (and (is-button? dom-node)
-                                                     (text-matches regex dom-node))))
+;(defn is-button-with-label? [string dom-node] (let [regex (js/RegExp. string)]
+;                                                (and (is-button? dom-node)
+;                                                     (text-matches regex dom-node))))
+;
+;(defn find-button-with-label [string dom-node] (let [predicate (partial is-button-with-label? string)]
+;                                                 (gd/findNode dom-node predicate)))
 
-(defn find-button-with-label [string dom-node] (let [predicate (partial is-button-with-label? string)]
-                                                 (gd/findNode dom-node predicate)))
-
-(defn find-element [keyword value elem]
-  (let [attr (name keyword)
-        selector (str/join ["[" attr "=" value "]"])]
-    (.querySelector elem selector)))
 
 (defn get-dom-element [component]
   ; TODO: ask tony how to shorten calls to TestUtils...
   (let [element (js/React.addons.TestUtils.renderIntoDocument component)]
     (when (js/React.addons.TestUtils.isDOMComponent element) (.getDOMNode element))))
+
+(defn find-element
+  "
+  Finds an HTML element inside a React component or HTML element based on keyword
+
+  Parameters:
+  *`keyword`: defines search type and can be one of:
+    *`:key`: the :key hash of the React component
+    *`:text`: the user-visible text of an element
+    *`:selector`: any arbitrary CSS selector
+    * any attribute name passed as a keyword, i.e. :class
+  *`value`: a string used to find the element based on your :keyword search type
+  *`obj`: should be either a React component or rendered HTML element
+
+  Returns a rendered HTML element or nil if no match is found.
+  "
+  [keyword value obj]
+  (let [elem (if (gd/isElement obj) obj (get-dom-element obj))]
+    (cond
+      (= keyword :key) (.querySelector elem (str/join ["[data-reactid$='$" value "'"]))
+      (= keyword :text) (gd/findNode elem (partial text-matches value elem))
+      (= keyword :selector) (or (.querySelector elem value) nil)
+      :else (let [attr (name keyword)
+                  selector (str/join ["[" attr "=" value "]"])]
+              (or (.querySelector elem selector) nil)))))
+
 
 ;(defn first-in-dom [search-kind pattern component]
 ;  nil)
--- test/dom_tools/query_spec.cljs
+++ test/dom_tools/query_spec.cljs
@@ -20,15 +20,30 @@
 ;
 ;(q/dom-frag (Button :button root-context))
 
-(def dumb-butt (d/button {:className "test-button" :data-foo "test-foo-data"} "derp."))
+(def dumb-butt (d/button {:className "test-button" :data-foo "test-foo-data" :key "myid"} "derp."))
 (def nested-button (d/div {} dumb-butt))
+(def dumb-div (d/div {}))
 
-(deftest get-dom-element-gets-dom-node-from-component
+(deftest get-dom-element
   (let [dom-frag (q/get-dom-element dumb-butt)]
-    (is (gd/isElement dom-frag))))
+    (is (gd/isElement dom-frag) "gets the dom node from a component")))
 
-(deftest find-element-gets-child-element-when-given-attr-and-value-and-parent-node
+(deftest find-element
   (let [component (js/React.addons.TestUtils.renderIntoDocument nested-button)
         parent-elem (.getDOMNode component)
-        child-elem (q/find-element :class "test-button" parent-elem)]
-    (is (= "derp." (.-innerHTML child-elem)))))
+        wrong-key (q/find-element :key "cyid" parent-elem)
+        child-elem-by-key (or (q/find-element :key "myid" parent-elem) dumb-div)
+        no-such-value (q/find-element :class "active" parent-elem)
+        no-such-attr (q/find-element :no-such-attr "myid" parent-elem)
+        child-elem-by-selector (or (q/find-element :selector "div .test-button" parent-elem) dumb-div)
+        child-elem-by-text (or (q/find-element :text "der" parent-elem) dumb-div)
+        child-elem-by-attr (or (q/find-element :class "test-button" parent-elem) dumb-div)]
+    (is (nil? wrong-key) "find-element returns nil when looking for the wrong key")
+    (is (nil? no-such-value) "find-element returns nil when a valid value is not found on an existing attribute")
+    (is (nil? no-such-attr) "find-element returns nil when an attribute is not found")
+    (is (= "derp." (.-innerHTML child-elem-by-key)) "find-element by data-reactid key")
+    (is (= "derp." (.-innerHTML child-elem-by-text)) "find-element by visible inner text")
+    (is (= "derp." (.-innerHTML child-elem-by-selector)) "find-element by arbitrary CSS selector")
+    (is (= "derp." (.-innerHTML child-elem-by-attr)) "find-element by attr and value")))
+
+
