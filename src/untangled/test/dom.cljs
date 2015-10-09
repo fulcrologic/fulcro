1bb0722680be7f85accfc385898b132e9558d7de
Ethan Gracer <EDGracer@thenavisway.com>
2015-10-09 11:06:50 -0700

    Story ID: RPC-212 Added has-attribute, has-attribute-value, and has-class assertions to untangled.test.dom

--- src/untangled/test/dom.cljs
+++ src/untangled/test/dom.cljs
@@ -1,7 +1,8 @@
 (ns untangled.test.dom
   (:require [clojure.string :as str]
             [cljs.test :as t :include-macros true]
-            [goog.dom :as gd]))
+            [goog.dom :as gd]
+            goog.dom.classlist))
 
 
 (defn get-attribute
@@ -47,10 +48,12 @@
 
 (defn find-element
   "
-  Finds an HTML element inside a React component or HTML element based on keyword
+  Finds an HTML element inside a React component or HTML element based on a keyword.
 
   Parameters:
-  *`element`: A rendered HTML element, as created with `render-as-dom`.
+  *`element`: A rendered HTML element, as created with `render-as-dom`. Note that when an untangled application is
+    in test-mode, the rendered DOM is wrapped in a div created in untangled.core. So this function will search your
+    entire component, not just its children.
   *`keyword`: defines search type and can be one of:
     *`:key`: the :key hash of the React component, this will look for your `value` as a substring within a data-reactid
              attribute
@@ -66,9 +69,7 @@
     (cond
       (re-find #"-text$" keyword-str)
       (let [tagname (str/lower-case (re-find #"^\w+" keyword-str))]
-        (if (and (node-contains-text? value element) (= tagname (tag-name element)))
-          element
-          (gd/findNode element (fn [e] (and (node-contains-text? value e) (= tagname (tag-name e)))))))
+        (gd/findNode element (fn [e] (and (node-contains-text? value e) (= tagname (tag-name e))))))
       (= keyword :key) (.querySelector element (str/join ["[data-reactid$='$" value "'"]))
       (= keyword :class) (.querySelector element (str "." value))
       (= keyword :selector) (or (.querySelector element value) nil)
@@ -86,34 +87,61 @@
       (t/do-report {:type :fail :actual (gd/getTextContent ele) :expected text-or-regex})
       (t/do-report {:type :pass})
       )
-    (t/do-report {:type     :error :message (str "Could not find element " search-kind " " search-param)
-                  :expected "DOM element" :actual "Nil"
-                  })
+    (t/do-report {:type     :error :message (str "Could not find DOM element")
+                  :expected (str "DOM element specified by " search-kind " " search-param) :actual "Nil"})
     )
   )
 
 (defn has-element
-  "Same assertion as has-visible-text, but doesn't check the component's innerHTML. Uses find-element to
-  process search-kind and search-param on the given dom, then asserts that the returned element is not nil."
+  "A test assertion that uses find-element to process search-kind and search-param on the given dom, then
+  asserts that the returned element is not nil."
   [search-kind search-param dom]
   (if (find-element search-kind search-param dom)
     (t/do-report {:type :pass})
-    (t/do-report {:type     :error :message (str "Could not find element " search-kind " " search-param)
-                  :expected "DOM element" :actual "Nil"})))
+    (t/do-report {:type     :error :message (str "Could not find DOM element")
+                  :expected (str "DOM element specified by " search-kind " " search-param) :actual "Nil"})))
 
-(defn has-attribute-value [attr attr-value search-kind search-param dom]
-  "Doc string"
+(defn has-attribute [attr search-kind search-param dom]
+  "A test assertion that uses find-element to process search-kind and search-param on the given dom, then
+  asserts that the given element contains the attribute passed in 'attr' as a keyword. This assertion
+  DOES NOT test that an attribute has a given value, just that the attribute exists (e.g. 'checked' on an
+  input checkbox). To assert that an attribute has a given value, use has-attribute-value."
   (if-let [ele (find-element search-kind search-param dom)]
-    (if (= (get-attribute ele attr) attr-value)
+    (if (get-attribute ele attr)
       (t/do-report {:type :pass})
-      (t/do-report {:type :fail :actual (get-attribute ele attr) :expected attr-value})
-      )
-    (t/do-report {:type     :error :message (str "Could not find element " search-kind " " search-param)
-                  :expected "DOM element" :actual "Nil"})))
-
-(defn has-class []
-
+      (t/do-report {:type   :fail
+                    :actual "Attribute not found" :expected (name attr)}))
+    (t/do-report {:type     :error :message (str "Could not find DOM element")
+                  :expected (str "DOM element specified by " search-kind " " search-param) :actual "Nil"}))
   )
+
+(defn has-attribute-value [attr desired-value search-kind search-param dom]
+  "A test assertion that uses find-element to process search-kind and search-param on the given dom, then
+  asserts that the given element contains the attribute 'attr=value', where 'attr' is a keyword and
+  'value' is a string. DO NOT use this function to test for attributes without an assigned value
+  (e.g. 'checked' on an input checkbox), use has-attribute instead."
+  (if-let [ele (find-element search-kind search-param dom)]
+    (if-let [actual-value (get-attribute ele attr)]
+      (if (= actual-value desired-value)
+        (t/do-report {:type :pass})
+        (t/do-report {:type   :fail
+                      :actual (str (name attr) "=" actual-value) :expected (str (name attr) "=" desired-value)}))
+      (t/do-report {:type     :error :message (str "Could not find attribute")
+                    :expected (str (name attr) "=" (if (nil? desired-value) "nil" desired-value))
+                    :actual   (str "Attribute '" (name attr) "' not found")}))
+    (t/do-report {:type     :error :message (str "Could not find DOM element")
+                  :expected (str "DOM element specified by " search-kind " " search-param) :actual "Nil"})))
+
+(defn has-class [class-name search-kind search-param dom]
+  "A test assertion that uses find-element to process search-kind and search-param on the given dom, then
+  asserts that the given element contains a class named 'class-name', where 'class-name is a string."
+  (if-let [ele (find-element search-kind search-param dom)]
+    (if (.contains gd/classlist ele class-name)
+      (t/do-report {:type :pass})
+      (t/do-report {:type   :fail
+                    :actual (str "Class '" class-name "' not found in DOM element specified by " search-kind " " search-param) :expected (str class-name " found in class attribute.")}))
+    (t/do-report {:type     :error :message (str "Could not find DOM element")
+                  :expected (str "DOM element specified by " search-kind " " search-param) :actual "Nil"})))
 
 (defn has-selected-option [search-type search-value dom-with-select selected-value]
   (if-let [ele (find-element search-type search-value dom-with-select)]
