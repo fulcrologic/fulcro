857e356366dcf3363585a502892705e7003ac5d5
Jeff Slavin <jdslavin@thenavisway.com>
2015-11-19 17:18:28 -0800

    - corrected issue with version of untangled so i can match the tags

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject untangled "0.3.1"
+(defproject untangled "0.3.2"
   :description "An opinionated data model for use with (and following) the sensibilities of Quiescent."
   :url ""
   :license {:name "NAVIS"
