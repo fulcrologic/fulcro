e7fe5cd8e804da16ea69b819131fd7fdc2b65c17
Tony Kay <twkay@thenavisway.com>
2015-12-15 16:35:16 -0800

    Story ID: GEM-102 fixed deps to properly declare scopes

--- project.clj
+++ project.clj
@@ -1,14 +1,18 @@
-(defproject untangled-datomic-helpers "0.2.5-SNAPSHOT"
+(defproject untangled-datomic-helpers "0.2.4-SNAPSHOT"
   :description "Support for Datomic, including extensions to schema validation and tracked Datomic schema migrations."
   :url ""
-  :dependencies [[org.clojure/clojure "1.7.0"]
+  :dependencies [[org.clojure/clojure "1.7.0" :scope "provided"]
                  [com.datomic/datomic-pro "0.9.5206" :exclusions [joda-time] :scope "provided"]
                  [io.rkn/conformity "0.3.4" :exclusions [com.datomic/datomic-free]]
                  [datomic-helpers "1.0.0"]
                  [org.clojure/tools.namespace "0.2.10"]
+                 [commons-codec "1.6"]
                  [com.stuartsierra/component "0.2.3"]
                  [com.taoensso/timbre "4.1.4"]
                  [org.clojure/java.classpath "0.2.2"]
+                 [untangled-spec "0.2.1" :scope "test" :exclusions [org.clojure/google-closure-library-third-party org.clojure/google-closure-library io.aviso/pretty org.clojure/clojurescript]]
+                 [crypto-password "0.1.3" :scope "test"]
+                 [midje "1.8.2" :scope "test" :exclusions [org.clojure/clojure joda-time clj-time]]
                  [com.rpl/specter "0.8.0"]]
 
   :repositories [["releases" "https://artifacts.buehner-fry.com/artifactory/internal-release"]
@@ -29,9 +33,4 @@
   {
    :dev {
          :source-paths ["env/dev"]
-         :dependencies [
-                        [org.clojure/tools.nrepl "0.2.10"]
-                        [untangled-spec "0.1.1"]
-                        [crypto-password "0.1.3"]
-                        [midje "1.6.3"]]
          :repl-options {:init-ns user}}})
