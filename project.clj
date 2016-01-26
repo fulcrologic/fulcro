c37b1de9f14b7344a0dc6825672806aa4105ab2e
Tony Kay <twkay@thenavisway.com>
2016-01-25 16:15:22 -0800

    fixed project file for artifact deployment

--- project.clj
+++ project.clj
@@ -24,6 +24,7 @@
 
   :clean-targets ^{:protect false} ["resources/private/js" "resources/public/js/test" "resources/public/js/compiled" "target"]
   :source-paths ["src" "spec"]
+  :resource-paths ["src" "resources"]
 
   :plugins [[lein-cljsbuild "1.1.2"]
             [lein-doo "0.1.6"]
