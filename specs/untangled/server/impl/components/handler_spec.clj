89eed3e74b2894a9b5e49f9068630ee5ecbc71e3
Ethan Gracer <EDGracer@thenavisway.com>
2016-01-27 17:16:30 -0800

    Story ID: GEM-200 Summary: - Finished writing tests for the api endpoint - Wrote out specs for generate-response

--- specs/untangled/server/impl/components/handler_spec.clj
+++ specs/untangled/server/impl/components/handler_spec.clj
@@ -1,14 +1,24 @@
 (ns ^:focused untangled.server.impl.components.handler-spec
   (:require [untangled-spec.core :refer [specification when-mocking assertions component behavior]]
             [untangled.server.impl.components.handler :as h]
-            [om.next.server :as om]))
+            [om.next.server :as om]
+            [taoensso.timbre :as timbre])
+  (:import (clojure.lang ExceptionInfo)))
+
+(specification "generate-response"
+  (behavior "returns a map with status, header, and body.")
+  (behavior "merges Content-Type of transit json to the passed-in headers.")
+  (behavior "does not permit"
+    (behavior "a \"Content-Type\" key in the header.")
+    (behavior "a status code less than 100.")
+    (behavior "a status code greater than or equal to 600.")))
 
 (specification "API Endpoint"
   (let [my-read (fn [_ key _] {:value (case key
                                         :foo "success"
                                         :bar (throw (ex-info "Oops" {:my :bad}))
                                         :bar' (throw (ex-info "Oops'" {:status 402 :body "quite an error"}))
-                                        :baz (throw (Exception.)))})
+                                        :baz (throw (Exception. "My Exception")))})
 
         parser (om/parser {:read my-read})
         parse-result (fn [query] (h/api {:parser parser :transit-params query}))]
@@ -34,12 +44,19 @@
               (:body result) => "quite an error")))
 
         (behavior "when the parser generates an unexpected error"
-          (let [result (parse-result [:bar])]))
+          (let [result (parse-result [:bar])]
+            (assertions
+              "returns a 500 http status code."
+              (:status result) => 500
+
+              "contains an exception in the response body."
+              (:body result) =fn=> (partial instance? ExceptionInfo))))
 
         (behavior "when the parser does not generate the error"
-          (assertions
-            "returns a status code == 500"
-            false => true
+          (let [result (parse-result [:baz])]
+            (assertions
+              "returns a 500 http status code"
+              (:status result) => 500
 
-            "returns exception data in the response body"
-            false => true))))))
+              "returns exception data in the response body"
+              (str (:body result)) => "java.lang.Exception: My Exception")))))))
