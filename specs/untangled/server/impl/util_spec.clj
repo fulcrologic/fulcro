019910389eb4eaf9b090f3f2c8aa057479bcda24
Ethan Gracer <EDGracer@thenavisway.com>
2016-01-28 11:21:20 -0800

    Story ID: GEM-200 Summary: - Added more complex tests for strip-parameters

--- specs/untangled/server/impl/util_spec.clj
+++ specs/untangled/server/impl/util_spec.clj
@@ -19,8 +19,26 @@
     (assertions
       "parameterized prop reads"
       (n/strip-parameters `[(:some/key {:arg :foo})]) => [:some/key]
+
       "parameterized join reads"
       (n/strip-parameters `[({:some/key [:sub/key]} {:arg :foo})]) => [{:some/key [:sub/key]}]
-      "paremeterized mutations"
-      (n/strip-parameters ['(fire-missiles! {:arg :foo})]) => '[fire-missiles!])))
+
+      "nested parameterized join reads"
+      (n/strip-parameters `[{:some/key [({:sub/key [:sub.sub/key]} {:arg :foo})]}]) => [{:some/key [{:sub/key [:sub.sub/key]}]}]
+
+      "multiple parameterized reads"
+      (n/strip-parameters
+        `[(:some/key {:arg :foo})
+          :another/key
+          {:non-parameterized [:join]}
+          {:some/other [{:nested [(:parameterized {:join :just-for-fun})]}]}])
+      =>
+      [:some/key :another/key {:non-parameterized [:join]} {:some/other [{:nested [:parameterized]}]}]
+
+      "parameterized mutations"
+      (n/strip-parameters ['(fire-missiles! {:arg :foo})]) => '[fire-missiles!]
+
+      "multiple parameterized mutations"
+      (n/strip-parameters ['(fire-missiles! {:arg :foo})
+                           '(walk-the-plank! {:right :now})]) => '[fire-missiles! walk-the-plank!])))
 
