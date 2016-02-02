9195d1cf618c30d4b31d2e3c1c30881aff5bd3c7
Ethan Gracer <EDGracer@thenavisway.com>
2016-02-01 17:14:57 -0800

    Story ID: GEM-219 Summary: - Added doc strings for client-side protocol testing - updated specs for new function names

--- src/untangled/client/protocol_support.cljs
+++ src/untangled/client/protocol_support.cljs
@@ -25,8 +25,8 @@
    does not contain an entry for it."
   [state tid->rid & [pred]]
   (clojure.walk/prewalk #(if ((or pred tempid?) %)
-                           (get tid->rid % %) %)
-                        state))
+                          (get tid->rid % %) %)
+    state))
 
 (defn protocol-data
   "Returns a function "
@@ -42,7 +42,7 @@
   [new-state delta]
   (if (empty? delta)
     (throw (ex-info "Cannot have empty :merge-delta"
-                    {:new-state new-state}))
+             {:new-state new-state}))
     (doseq [[key-path value] delta]
       (assertions
         (get-in new-state key-path) => value))))
@@ -55,16 +55,21 @@
     (into {} (map #(vector % (om/tempid)) @allocated-ids))))
 
 (defui Root
-       static om/IQuery (query [this] [:fake])
-       Object (render [this] (dom/div nil "if you see this something is wrong")))
+  static om/IQuery (query [this] [:fake])
+  Object (render [this] (dom/div nil "if you see this something is wrong")))
 
 (defn- init-testing []
   (-> (core/new-untangled-client :started-callback #() :networking #())
-      (core/mount Root "invisible-specs")))
+    (core/mount Root "invisible-specs")))
+
+;;; Public API
 
 (defn check-optimistic-update
-  "`app` is an UntangledApplication
-   `data` is a map containing protocoly stuff"
+  "Takes a map containing:
+  `initial-ui-state`: denormalized app state prior to the optimistic update for transactions going to the server
+  `ui-tx`: the om transaction that modifies the app state prior to receiving a server response
+  `optimistic-delta`: the expected changes to the app state after executing ui-tx. See Protocol Testing README for how
+  to build this properly."
   [{:keys [initial-ui-state ui-tx optimistic-delta] :as data}]
   (component "Optimistic Updates"
     (let [{:keys [parser]} (init-testing)
@@ -75,12 +80,14 @@
       (behavior "trigger correct state transitions"
         (parse ui-tx)
         (check-delta (rewrite-tempids @state (set/map-invert tempid-map)
-                                      omt/tempid?)
-                     optimistic-delta)))))
+                       omt/tempid?)
+          optimistic-delta)))))
 
 (defn check-server-tx
-  "`app` is an UntangledApplication
-   `data` is a map containing protocoly stuff"
+  "Takes a map containing:
+  `initial-ui-state`: denormalized app state prior to sending the server transaction
+  `ui-tx`: the om transaction that modifies the app state locally
+  `server-tx`: the server transaction corresponding to ui-tx"
   [{:keys [initial-ui-state ui-tx server-tx] :as data}]
   (component "Client Remoting"
     (let [{:keys [parser]} (init-testing)
@@ -90,13 +97,18 @@
           ui-tx (rewrite-tempids ui-tx tempid-map)]
       (assertions "Generates the expected server query"
         (-> (parse ui-tx :remote)
-            plumbing/filter-loads-and-fallbacks
-            plumbing/strip-ui
-            (rewrite-tempids (set/map-invert tempid-map)
-                             omt/tempid?))
+          plumbing/filter-loads-and-fallbacks
+          plumbing/strip-ui
+          (rewrite-tempids (set/map-invert tempid-map)
+            omt/tempid?))
         => server-tx))))
 
-(defn check-response
+(defn check-response-from-server
+  "Takes a map containing:
+  `response`: the exact data the server sends back to the client
+  `pre-response-state`: normalized state prior to receiving `response`
+  `server-tx`: the transaction originally sent to the server, yielding `response`
+  `merge-delta`: the delta between `pre-response-state` and its integration with `response`"
   [{:keys [response pre-response-state server-tx merge-delta]}]
   (component "Server response"
     (let [{:keys [reconciler]} (init-testing)
