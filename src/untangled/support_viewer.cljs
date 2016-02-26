44654f776815c1830ac1c44e49064a1e6af61f22
Tony Kay <twkay@thenavisway.com>
2016-02-26 12:15:28 -0800

    fixed up mutations on history viewer DRY

--- src/untangled/support_viewer.cljs
+++ src/untangled/support_viewer.cljs
@@ -4,18 +4,22 @@
     [om.dom :as dom]
     [untangled.client.core :as core]
     [untangled.client.mutations :as m]
+    [yahoo.intl-messageformat-with-locales]
+    [untangled.i18n :refer-macros [tr trf]]
     [untangled.client.impl.network :as net]))
 
 (defui ^:once SupportViewerRoot
   static om/IQuery
-  (query [this] [:react-key])
+  (query [this] [:react-key :current-position :client-time :frames])
   Object
   (render [this]
-    (let [{:keys [react-key] :or {react-key "ROOT"}} (om/props this)]
-      (dom/div #js {:key react-key}
-        (dom/button #js {:onClick #(om/transact! this '[(support-viewer/step-back)])} "<Back")
-        " "
-        (dom/button #js {:onClick #(om/transact! this '[(support-viewer/step-forward)])} "Forward>")))))
+    (let [{:keys [react-key current-position client-time frames] :or {react-key "ROOT"}} (om/props this)]
+      (dom/div #js {:key react-key :className "history-controls"}
+        (dom/button #js {:onClick #(om/transact! this '[(support-viewer/step-back)])} (tr "<Back"))
+        (dom/button #js {:onClick #(om/transact! this '[(support-viewer/step-forward)])} (tr "Forward>"))
+        (dom/hr nil)
+        (dom/span #js {:className "frame"} (trf "Frame {f,number} of {end,number} " :f (inc current-position) :end frames))
+        (dom/span #js {:className "timestamp"} (trf "{ts,date,short} {ts,time,long}" :ts client-time))))))
 
 (defn history-entry [history n]
   (let [steps (:steps history)
@@ -30,7 +34,7 @@
       (do (core/refresh this) this)
       (do
         (reset! application (core/mount @application app-root dom-id))
-        (core/reset-state! @application (history-entry history 100000))
+        (core/reset-state! @application (history-entry history (-> history :steps count dec)))
         (reset! support (core/mount @support SupportViewerRoot dom-id-or-node))
         @support)))
 
@@ -52,20 +56,25 @@
                                     :support     (atom (core/new-untangled-client
                                                          :initial-state {:history          history
                                                                          :application      app
+                                                                         :client-time      (js/Date.)
+                                                                         :frames           (inc max-idx)
                                                                          :current-position max-idx}
                                                          :networking (net/mock-network)))})]
     (core/mount viewer SupportViewerRoot support-dom-id)))
 
-(defmethod m/mutate 'support-viewer/step-forward [{:keys [state]} k params]
-  {:action (fn []
-             (let [{:keys [application current-position history]} @state
-                   max-idx (dec (count (:steps history)))]
-               (swap! state update :current-position (fn [p] (if (< p max-idx) (inc p) p)))
-               (core/reset-state! @application (history-entry history (:current-position @state)))))})
+(defn history-step [state delta-fn]
+  (let [{:keys [application history]} @state
+        max-idx (dec (count (:steps history)))
+        p (:current-position @state)
+        new-pos (min max-idx (max 0 (delta-fn p)))
+        entry (history-entry history new-pos)
+        tm (-> entry :untangled/meta :client-time)]
+    (swap! state (fn [s] (-> s
+                           (assoc :current-position new-pos)
+                           (assoc :client-time tm))))
+    (core/reset-state! @application entry)))
 
-(defmethod m/mutate 'support-viewer/step-back [{:keys [state]} k params]
-  {:action (fn []
-             (let [{:keys [application current-position history]} @state]
-               (swap! state update :current-position (fn [p] (if (> p 0) (dec p) p)))
-               (core/reset-state! @application (history-entry history (:current-position @state)))))})
+(defmethod m/mutate 'support-viewer/step-forward [{:keys [state]} k params] {:action #(history-step state inc)})
+
+(defmethod m/mutate 'support-viewer/step-back [{:keys [state]} k params] {:action #(history-step state dec)})
 
