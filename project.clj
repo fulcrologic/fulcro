485a3befe3f2a4dad3e3768d3995a06ff9cfa8c0
Anthony D'Ambrosio <anthony.dayzerostudio@gmail.com>
2016-02-29 17:03:22 -0800

    0.4.5-SNAPSHOT
    somebody did a doo-doo and bumped 0.4.4 to 0.4.4-SNAPSHOT...

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject navis/untangled-server "0.4.4-SNAPSHOT"
+(defproject navis/untangled-server "0.4.5-SNAPSHOT"
   :description "Library for creating Untangled web servers"
   :url ""
   :dependencies [[org.clojure/clojure "1.7.0" :scope "provided"]
