bd9126e2592bab32ca9e9f6f97a6813c626fc0ac
Ethan Gracer <EDGracer@thenavisway.com>
2016-03-03 09:18:04 -0800

    Story ID: develop Summary: - updated app/change-locale mutation to change top-level react key

--- src/untangled/client/mutations.cljs
+++ src/untangled/client/mutations.cljs
@@ -1,5 +1,6 @@
 (ns untangled.client.mutations
   (:require [om.next :as om]
+            [untangled.dom :refer [unique-key]]
             [untangled.client.impl.data-fetch :as df]
             [untangled.i18n.core :as i18n]
             [untangled.client.logging :as log]))
@@ -72,8 +73,10 @@
 (defmethod mutate 'app/clear-error [{:keys [state]} _ _]
   {:action #(swap! state assoc :last-error nil)})
 
-(defmethod mutate 'app/change-locale [_ _ {:keys [lang]}]
-  (reset! i18n/*current-locale* lang))
+(defmethod mutate 'app/change-locale [{:keys [state]} _ {:keys [lang]}]
+  {:action (fn []
+             (reset! i18n/*current-locale* lang)
+             (swap! state assoc :react-key (unique-key)))})
 
 (defmethod mutate 'tx/fallback [env _ {:keys [action execute] :as params}]
   (if execute
