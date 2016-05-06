0849b048312b0d30402817269dedd034f60b6e3b
Karan <currentoor@gmail.com>
2016-05-05 17:30:57 -0700

    Allow all the http-kit options to pass through.

--- src/untangled/server/impl/components/web_server.clj
+++ src/untangled/server/impl/components/web_server.clj
@@ -8,21 +8,20 @@
 (defrecord WebServer [port handler server]
   component/Lifecycle
   (start [component] []
-    (timbre/info "Web server started successfully."
-      (when (env :dev) "(using the development profile)"))
-    (try
-      (assoc component :server (run-server (:all-routes (:handler component))
-                                 {:port (-> component :config :value :port) :join? false}))
-      (catch Exception e
-        (timbre/fatal "Failed to start web server " e))
-      )
-    )
+    (let [http-kit-opts [:ip :port :thread :worker-name-prefix
+                         :queue-size :max-body :max-line]
+          server-opts (select-keys (-> component :config :value)
+                                   http-kit-opts)]
+      (timbre/info (str "Web server started successfully."
+                        (if (env :dev) (timbre/info "(using the development profile)")))
+                   "With options:" server-opts)
+      (try
+        (assoc component :server (run-server (:all-routes (:handler component))
+                                             server-opts))
+        (catch Exception e
+          (timbre/fatal "Failed to start web server " e)))))
   (stop [component] []
     (when-let [server (:server component)]
       (server)
       (timbre/info "web server stopped.")
-      (assoc component :server nil))
-    )
-  )
-
-
+      (assoc component :server nil))))
