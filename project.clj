a1e24aa026814344de8c15746eeb3bcb006129d1
Tony Kay <twkay@thenavisway.com>
2016-05-20 11:56:44 -0700

    fixed version

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject navis/untangled-client "0.4.9"
+(defproject navis/untangled-client "0.4.10"
   :description "Client-side code for Untangled Webapps"
   :url ""
   :license {:name "NAVIS"
