27853fcd5113e2ce4fae2c603c7d9994c15a8729
Anthony D'Ambrosio <anthony.dayzerostudio@gmail.com>
2016-05-25 10:39:09 -0700

    Cleaning up and testing WebServer component

--- specs/untangled/server/impl/components/web_server_spec.clj
+++ specs/untangled/server/impl/components/web_server_spec.clj
@@ -1,20 +1,32 @@
 (ns untangled.server.impl.components.web-server-spec
-  (:require [com.stuartsierra.component :as component]
+  (:require [untangled-spec.core :refer
+             [specification component behavior assertions when-mocking provided]]
+            [com.stuartsierra.component :as component]
             [untangled.server.impl.components.web-server :as src]
             [untangled.server.core :refer [make-web-server]]
-            [untangled-spec.core :refer
-             [specification component behavior assertions when-mocking provided]]))
+            [org.httpkit.server :refer [run-server]]))
 
-(defn make-test-system []
-  (component/system-map
-    :config {:value {:port 1337}}
-    :handler {:all-routes :fake/all-routes}
-    :web-server (make-web-server)))
+(def dflt-cfg {:port 1337})
+
+(defn make-test-system
+  ([] (make-test-system dflt-cfg))
+  ([cfg]
+   (component/system-map
+     :config {:value cfg}
+     :handler {:all-routes :fake/all-routes}
+     :web-server (make-web-server))))
 
 (specification "WebServer"
   (component "start"
     (behavior "correctly grabs the port & all-routes, and returns the started server under :server"
       (when-mocking
-        (org.httpkit.server/run-server routes options) => :ok
+        (run-server :fake/all-routes {:port 1337}) => :ok
         (assertions
-          (-> (make-test-system) .start :web-server :server) => :ok)))))
+          (-> (make-test-system) .start :web-server :server) => :ok)))
+    (behavior "only allows http-kit-opts to be passed to the server"
+      (let [ok-cfg (zipmap src/http-kit-opts (mapv (constantly 42) src/http-kit-opts))
+            ok-cfg+bad (merge ok-cfg {:not-in/http-kit-opts :bad/value})]
+        (when-mocking
+          (run-server :fake/all-routes opts) => (do (assertions opts => ok-cfg) :ok)
+          (assertions
+            (-> (make-test-system ok-cfg+bad) .start :web-server :server) => :ok))))))
--- src/untangled/server/impl/components/web_server.clj
+++ src/untangled/server/impl/components/web_server.clj
@@ -4,25 +4,24 @@
             [com.stuartsierra.component :as component])
   (:gen-class))
 
+(def http-kit-opts
+  [:ip :port :thread :worker-name-prefix
+   :queue-size :max-body :max-line])
+
 (defrecord WebServer [port handler server]
   component/Lifecycle
-  (start [component] []
+  (start [this]
     (try
-      (let [http-kit-opts [:ip :port :thread :worker-name-prefix
-                           :queue-size :max-body :max-line]
-            server-opts (select-keys (-> component :config :value)
-                                     http-kit-opts)
-            configured-port (:port server-opts)
-            rv (assoc component
-                 :port configured-port
-                 :server (run-server (:all-routes (:handler component))
-                                     server-opts))]
-        (timbre/info (str "Web server started successfully. Configured options: " server-opts))
-        rv)
+      (let [server-opts (select-keys (-> this :config :value) http-kit-opts)
+            port (:port server-opts)
+            started-server (run-server (:all-routes handler) server-opts)]
+        (timbre/info "Web server started successfully. With options:" server-opts)
+        (assoc this :port port :server started-server))
       (catch Exception e
-        (timbre/fatal "Failed to start web server " e))))
-  (stop [component] []
-    (when-let [server (:server component)]
+        (timbre/fatal "Failed to start web server " e)
+        (throw e))))
+  (stop [this]
+    (when server
       (server)
       (timbre/info "web server stopped.")
-      (assoc component :server nil))))
+      (assoc this :server nil))))
