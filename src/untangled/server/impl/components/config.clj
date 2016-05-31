5d429f1d96a936269ec73dc0e127a418654db43a
Anthony D'Ambrosio <anthony.dayzerostudio@gmail.com>
2016-05-30 18:43:46 -0700

    logging the config on start

--- src/untangled/server/impl/components/config.clj
+++ src/untangled/server/impl/components/config.clj
@@ -3,7 +3,8 @@
             [clojure.java.classpath :as cp]
             [clojure.java.io :as io]
             [com.rpl.specter :refer [transform walker]]
-            [clojure.edn :as edn])
+            [clojure.edn :as edn]
+            [taoensso.timbre :as log])
   (:import (java.io File)))
 
 (defn- get-system-prop [prop-name]
@@ -73,5 +74,6 @@
   component/Lifecycle
   (start [this]
     (let [config (or value (load-config {:config-path config-path}))]
+      (log/debug "Loaded config:" config)
       (assoc this :value config)))
   (stop [this] this))
