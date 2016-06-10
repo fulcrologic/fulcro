e190bba7a9a6cb31955bea62147d6be2420ad758
Tony Kay <twkay@thenavisway.com>
2016-06-10 12:38:09 -0700

    added merge-state!, reordered changelog, updated version number

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject navis/untangled-client "0.5.0"
+(defproject navis/untangled-client "0.5.1-SNAPSHOT"
   :description "Client-side code for Untangled Webapps"
   :url ""
   :license {:name "NAVIS"
