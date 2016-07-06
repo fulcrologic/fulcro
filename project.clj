8a0ecffae5a5de8ff1964c5c6d5e1b7d0247b9f5
Ethan Gracer <EDGracer@thenavisway.com>
2016-07-06 12:03:33 -0700

    Removed snapshot

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject navis/untangled-client "0.5.4-SNAPSHOT"
+(defproject navis/untangled-client "0.5.4"
   :description "Client-side code for Untangled Webapps"
   :url ""
   :license {:name "NAVIS"
