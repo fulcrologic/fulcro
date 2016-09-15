93ce258507385fbc4aced0f37aa266133ca2fabb
Jeff Slavin <jdslavin@thenavisway.com>
2016-09-15 15:20:12 -0700

    bump version for patch

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject navis/untangled-client "0.5.5-1"
+(defproject navis/untangled-client "0.5.6-SNAPSHOT"
   :description "Client-side code for Untangled Webapps"
   :url ""
   :license {:name "MIT"
