41382cad06853bcde407d8e4f1996fdcf613c94d
Tony Kay <tony.kay@gmail.com>
2016-09-15 22:26:47 -0700

    set version

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject untangled/om-css "0.0.1-SNAPSHOT"
+(defproject untangled/om-css "1.0.0"
   :description "A composable library for co-located CSS on Om/Untangled UI components"
   :url ""
   :license {:name "MIT"
