bfc1198b99487e9d6e20efaf870baa990dda6f83
Tony Kay <twkay@thenavisway.com>
2016-09-30 13:36:44 -0700

    updated yahoo messageformat lib

--- src/deps.cljs
+++ src/deps.cljs
@@ -1,3 +1,4 @@
 {:foreign-libs
  [{:file "yahoo/intl-messageformat-with-locales.js"
+   :file-min "yahoo/intl-messageformat-with-locales.min.js"
    :provides ["yahoo.intl-messageformat-with-locales"]}]}
