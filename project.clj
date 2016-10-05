8b1d9ace9f800f0213bbe9ce4a61bc9991c58e8b
Tony Kay <twkay@thenavisway.com>
2016-10-05 13:32:01 -0700

    updated version for next snap

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject navis/untangled-client "0.5.7"
+(defproject navis/untangled-client "0.5.8-SNAPSHOT"
   :description "Client-side code for Untangled Webapps"
   :url ""
   :license {:name "MIT"
