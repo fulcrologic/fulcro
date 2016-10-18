3e0f740a21a70f35dd7a800b2770968db95f0063
Tony Kay <tony.kay@gmail.com>
2016-10-17 22:14:34 -0700

    changed mount to re-add Om root. Helps with refresh of devcards

--- src/untangled/client/cards.cljc
+++ src/untangled/client/cards.cljc
@@ -11,5 +11,4 @@
          (defonce ~varname (atom (uc/new-untangled-client :initial-state state-atom# ~@args)))
          (reset! ~varname (uc/mount @~varname ~root-ui node#))
          ; ensures shows app state immediately if you're using inspect data and InitialAppState:
-         (js/setTimeout (fn [] (swap! state-atom# assoc :ui/react-key (udom/unique-key))) 200)
-         node#))))
+         (js/setTimeout (fn [] (swap! state-atom# assoc :ui/react-key (udom/unique-key))) 200)))))
--- src/untangled/client/core.cljs
+++ src/untangled/client/core.cljs
@@ -136,9 +136,12 @@
   (log/info "RERENDER: NOTE: If your UI doesn't change, make sure you query for :ui/react-key on your Root and embed that as :key in your top-level DOM element")
   (udom/force-render reconciler))
 
-(defn mount* [{:keys [mounted? initial-state reconciler-options] :as app} root-component dom-id-or-node]
+(defn mount* [{:keys [mounted? reconciler initial-state reconciler-options] :as app} root-component dom-id-or-node]
   (if mounted?
-    (do (refresh* app) app)
+    (do
+      (om/add-root! reconciler root-component dom-id-or-node)
+      (refresh* app)
+      app)
     (let [uses-initial-app-state? (implements? InitialAppState root-component)
           ui-declared-state (and uses-initial-app-state? (untangled.client.core/initial-state root-component nil))
           atom-supplied? (= Atom (type initial-state))
