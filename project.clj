3d6aa5546f522bbcc5aceb51104a5333e857089f
Tony Kay <twkay@thenavisway.com>
2016-11-16 09:17:06 -0800

    fixed return value handling from server as a mutation-merge function. Updated changelog and docstring

--- project.clj
+++ project.clj
@@ -4,26 +4,28 @@
   :license {:name "MIT"
             :url  "https://opensource.org/licenses/MIT"}
 
-  :dependencies [[org.clojure/clojure "1.8.0"]
-                 [org.clojure/clojurescript "1.9.229"]
+  :dependencies [[org.clojure/clojure "1.9.0-alpha14" :scope "provided"]
+                 [org.clojure/clojurescript "1.9.293" :scope "provided"]
                  [org.clojure/core.async "0.2.391"]
                  [differ "0.2.1"]
                  [devcards "0.2.2" :exclusions [org.omcljs/om org.omcljs/om org.clojure/core.async] :scope "provided"]
                  [lein-doo "0.1.7" :scope "test"]
                  [com.lucasbradstreet/cljs-uuid-utils "1.0.2"]
                  [navis/untangled-spec "0.3.9" :scope "test"]
-                 [org.omcljs/om "1.0.0-alpha46" :scope "provided"]]
+                 [org.omcljs/om "1.0.0-alpha47" :scope "provided"]]
 
   :jvm-opts ["-XX:-OmitStackTraceInFastThrow" "-Xmx512m" "-Xms256m"]
   :clean-targets ^{:protect false} ["resources/private/js" "resources/public/js/test" "resources/public/js/compiled" "target"]
 
-  :resource-paths ["src" "resources"] ; maven deploy to internal artifactory needs src here
+  :resource-paths ["src" "resources"]                       ; maven deploy to internal artifactory needs src here
 
   :plugins [[lein-cljsbuild "1.1.4"]
             [lein-doo "0.1.7"]]
 
   :doo {:build "automated-tests"
         :paths {:karma "node_modules/karma/bin/karma"}}
+
+  :figwheel {:server-port 8080}
 
   :cljsbuild {:builds
               [{:id           "test"
