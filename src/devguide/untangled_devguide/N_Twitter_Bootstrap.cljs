c998902c7431559b12c9e22b96fbacb74687e702
Tony Kay <tony.kay@gmail.com>
2017-05-26 23:34:58 -0700

    added more css supporting functions

--- src/devguide/untangled_devguide/N_Twitter_Bootstrap.cljs
+++ src/devguide/untangled_devguide/N_Twitter_Bootstrap.cljs
@@ -16,7 +16,12 @@
   "
   # Twitter Bootstrap
 
-  Untangled includes functions that emit the DOM with CSS for version 3 of Twitter's Bootstrap CSS and Components.")
+  Untangled includes functions that emit the DOM with CSS for version 3 of Twitter's Bootstrap CSS and Components.
+
+  Notice that the CSS affects many DOM elements, which means you'll see examples that use Om's DOM functions, which
+  in turn require a JavaScript object as the first argument (for performance). The helper functions from the
+  `bootstrap` namespace need to modify the incoming arguments, so the first argument (if it takes DOM props) is
+  a cljs map instead. ")
 
 (defn- col [attrs children] (b/col (assoc attrs :className "boxed") children))
 
@@ -81,15 +86,186 @@
             (col {:xs 6 :md 4} "xs 6 md 4"))
           (b/row {} (col {:xs 6} "xs 6") (col {:xs 6} "xs 6")))))))
 
-(defcard typography-lead
-  "Lead Text can be generated with `(b/lead {} \"Lead Text\")"
-  (render-example "100%" "100%"
+(defcard typography
+  "Various elements support modified typography."
+  (render-example "100%" "400px"
+    (b/container {}
+      (b/row nil
+        (b/col {:xs 6} "(b/lead {} \"Lead Text\")")
+        (b/col {:xs 6} (b/lead {} "Lead Text")))
+      (dom/hr nil)
+      (b/row nil
+        (b/col {:xs 6} "(dom/p #js {} \"Paragraph Text\")")
+        (b/col {:xs 6} (dom/p #js {} "Paragraph Text")))
+      (dom/hr nil)
+      (b/row nil
+        (b/col {:xs 6} "(dom/mark #js {} \"Highlighted Text\")")
+        (b/col {:xs 6} (dom/mark #js {} "Highlighted Text")))
+      (dom/hr nil)
+      (b/row nil
+        (b/col {:xs 6} "(dom/del #js {} \"Deleted Text\")")
+        (b/col {:xs 6} (dom/del #js {} "Deleted Text")))
+      (dom/hr nil)
+      (b/row nil
+        (b/col {:xs 6} "(dom/s #js {} \"Strikethrough Text\")")
+        (b/col {:xs 6} (dom/s #js {} "Strikethrough Text")))
+      (dom/hr nil)
+      (b/row nil
+        (b/col {:xs 6} "(dom/ins #js {} \"Inserted Text\")")
+        (b/col {:xs 6} (dom/ins #js {} "Inserted Text")))
+      (dom/hr nil)
+      (b/row nil
+        (b/col {:xs 6} "(dom/u #js {} \"Underlined Text\")")
+        (b/col {:xs 6} (dom/u #js {} "Underlined Text")))
+      (dom/hr nil)
+      (b/row nil
+        (b/col {:xs 6} "(dom/small #js {} \"Small Text\")")
+        (b/col {:xs 6} (dom/small #js {} "Small Text")))
+      (dom/hr nil)
+      (b/row nil
+        (b/col {:xs 6} "(dom/strong #js {} \"Bold Text\")")
+        (b/col {:xs 6} (dom/strong #js {} "Bold Text")))
+      (dom/hr nil)
+      (b/row nil
+        (b/col {:xs 6} "(dom/em #js {} \"Italic Text\")")
+        (b/col {:xs 6} (dom/em #js {} "Italic Text")))
+      (dom/hr nil)
+      (b/row nil
+        (b/col {:xs 6} "(b/address \"Some Company\" :street \"111 NW 1st St.\" :city-state \"Chicago, IL 43156\" :phone \"(316) 555-1212\")")
+        (b/col {:xs 6} (b/address "Some Company" :street "111 NW 1st St." :city-state "Chicago, IL 43156"
+                         :phone "(316) 555-1212")))
+      (dom/hr nil)
+      (b/row nil
+        (b/col {:xs 6} "(dom/blockquote nil (dom/p nil \"Block quoted Text\"))")
+        (b/col {:xs 6} (dom/blockquote nil (dom/p nil "Block quoted Text"))))
+      (dom/hr nil)
+      (b/row nil
+        (b/col {:xs 6} "(b/quotation {:source \"in Joe's Diary\" :credit \"Joe\"} (dom/p nil \"Some crap he said.\"))")
+        (b/col {:xs 6} (b/quotation {:source "in Joe's Diary" :credit "Joe"} (dom/p nil "Some crap he said."))))
+      (dom/hr nil)
+      (b/row nil
+        (b/col {:xs 6} "(b/quotation {:align :right :source \"in Joe's Diary\" :credit \"Joe\"} (dom/p nil \"Some crap he said.\"))")
+        (b/col {:xs 6} (b/quotation {:align :right :source "in Joe's Diary" :credit "Joe"} (dom/p nil "Some crap he said."))))
+      (dom/hr nil)
+      (b/row nil
+        (b/col {:xs 6} "(dom/ul #js {} (dom/li nil \"A\") (dom/li nil \"B\")")
+        (b/col {:xs 6} (dom/ul #js {} (dom/li nil "A") (dom/li nil "B"))))
+      (dom/hr nil)
+      (b/row nil
+        (b/col {:xs 6} "(b/plain-ul {} (dom/li nil \"A\") (dom/li nil \"B\")")
+        (b/col {:xs 6} (b/plain-ul {} (dom/li nil "A") (dom/li nil "B"))))
+      (dom/hr nil)
+      (b/row nil
+        (b/col {:xs 6} "(b/inline-ul {} (dom/li nil \"A\") (dom/li nil \"B\")")
+        (b/col {:xs 6} (b/inline-ul {} (dom/li nil "A") (dom/li nil "B")))))))
+
+
+(defcard formatting-code
+  "The look of formatted code."
+  (render-example "100%" "400px"
+    (b/container {}
+      (b/row nil
+        (b/col {:xs 6} "(dom/code #js {} \"pwd\")")
+        (b/col {:xs 6} (dom/code #js {} "pwd")))
+      (dom/hr nil)
+      (b/row nil
+        (b/col {:xs 6} "(dom/kbd #js {} \"CTRL-C\")")
+        (b/col {:xs 6} (dom/kbd #js {} "CTRL-C")))
+      (dom/hr nil)
+      (b/row nil
+        (b/col {:xs 6} "(dom/pre #js {} \"(map identity\\n  list)\")")
+        (b/col {:xs 6} (dom/pre #js {} "(map identity\n  list)")))
+      (dom/hr nil)
+      (b/row nil
+        (b/col {:xs 6} "(dom/samp #js {} \"This is supposed to represent computer output\")")
+        (b/col {:xs 6} (dom/samp #js {} "This is supposed to represent computer output")))
+      (dom/hr nil))))
+
+(defcard tables
+  "Bootstrip includes styles for tables. The `b/table` wrapper handles remember the classes for you."
+  (render-example "100%" "400px"
     (dom/div nil
-      (b/lead {} "Lead Text")
-      (dom/p {} "This is a regular paragraph"))))
+      (dom/h4 nil "A plain table (table class automatically added with `(b/table ...)`:")
+      (b/table {}
+        (dom/thead nil
+          (dom/tr nil
+            (dom/th nil "H1")
+            (dom/th nil "H2")
+            (dom/th nil "H3")))
+        (dom/tbody nil
+          (dom/tr nil
+            (dom/td nil "1")
+            (dom/td nil "2")
+            (dom/td nil "3"))
+          (dom/tr nil
+            (dom/td nil "1")
+            (dom/td nil "2")
+            (dom/td nil "3"))
+          (dom/tr nil
+            (dom/td nil "1")
+            (dom/td nil "2")
+            (dom/td nil "3"))))
+      (dom/h4 nil "A table with `(b/table { :styles #{:striped :hover} } ...)`:")
+      (b/table {:styles #{:striped :hover}}
+        (dom/thead nil
+          (dom/tr nil
+            (dom/th nil "H1")
+            (dom/th nil "H2")
+            (dom/th nil "H3")))
+        (dom/tbody nil
+          (dom/tr nil
+            (dom/td nil "1")
+            (dom/td nil "2")
+            (dom/td nil "3"))
+          (dom/tr nil
+            (dom/td nil "1")
+            (dom/td nil "2")
+            (dom/td nil "3"))
+          (dom/tr nil
+            (dom/td nil "1")
+            (dom/td nil "2")
+            (dom/td nil "3"))))
+      (dom/h4 nil "A table with `(b/table { :styles #{:bordered :condensed} } ...)`:")
+      (b/table {:styles #{:bordered :condensed}}
+        (dom/thead nil
+          (dom/tr nil
+            (dom/th nil "H1")
+            (dom/th nil "H2")
+            (dom/th nil "H3")))
+        (dom/tbody nil
+          (dom/tr nil
+            (dom/td nil "1")
+            (dom/td nil "2")
+            (dom/td nil "3"))
+          (dom/tr nil
+            (dom/td nil "1")
+            (dom/td nil "2")
+            (dom/td nil "3"))
+          (dom/tr nil
+            (dom/td nil "1")
+            (dom/td nil "2")
+            (dom/td nil "3")))))))
+
+(defcard form-fields
+  "Labelled fields for forms"
+  (render-example "100%" "400px"
+    (dom/div #js {}
+      (b/labeled-input {:id "name" :type "text"} "Name:")
+      (b/labeled-input {:id "address" :type "text" :error "Must not be empty!"} "Address:")
+      (b/labeled-input {:id "phone" :type "text" :success "You can leave this blank."} "Phone:")
+      (b/labeled-input {:id "email" :type "email" :help "Your primary email"} "Email:"))))
+
+(defcard form-fields
+  "Labelled fields for forms"
+  (render-example "100%" "400px"
+    (dom/div #js {:className "form-horizontal"}
+      (b/labeled-input {:id "name" :split 2 :type "text"} "Name:")
+      (b/labeled-input {:id "address" :split 2 :type "text" :error "Must not be empty!"} "Address:")
+      (b/labeled-input {:id "phone" :split 2 :type "text"} "Phone:")
+      (b/labeled-input {:id "email" :split 2 :type "email" :help "Your primary email"} "Email:"))))
 
 (defn render-example [width height & children]
-  (ele/ui-iframe {:height height :width width}
+  (ele/ui-iframe {:frameBorder 0 :height height :width width}
     (dom/div nil
       (dom/style nil ".boxed {border: 1px solid black}")
       (dom/link #js {:rel         "stylesheet"
--- src/main/untangled/ui/bootstrap.cljc
+++ src/main/untangled/ui/bootstrap.cljc
@@ -1,5 +1,7 @@
 (ns untangled.ui.bootstrap
-  (:require [om.dom :as dom]))
+  (:require [om.dom :as dom]
+            [clojure.string :as str]
+            [clojure.set :as set]))
 
 #?(:clj (defn- clj->js [m] m))
 
@@ -67,3 +69,96 @@
   [attrs & children]
   (p-with-class "lead" attrs children))
 
+(defn address
+  "Formats an address"
+  [name & {:keys [street street2 city-state phone email]}]
+  (let [brs      (repeatedly #(dom/br nil))
+        children (filter identity
+                   (list
+                     street
+                     street2
+                     city-state
+                     (when phone
+                       (list (dom/abbr #js {:title "Phone"} "P:") phone))))]
+    (apply dom/address nil
+      (dom/strong nil name) (dom/br nil)
+      (interleave children brs))))
+
+(defn quotation
+  "Render a block quotation with optional citation and source.
+
+  align - :right or :left (default)
+  credit - The name of the person.
+  source - The place where it was said/written, you supply the joining preposition.
+  children - The DOM element(s) (typicaly a p) that represent the body of the quotation
+  "
+  [{:keys [align credit source] :as attrs} & children]
+  (let [attrs   (cond-> (dissoc attrs :align :credit :source)
+                  (= :right align) (assoc :className (str "blockquote-reverse " (:className attrs))))
+        content (concat children [(dom/footer nil credit " " (dom/cite nil source))])]
+    (apply dom/blockquote (clj->js attrs) content)))
+
+(defn plain-ul
+  "Render an unstyled unordered list"
+  [attrs & children]
+  (let [attrs (update attrs :className #(str " list-unstyled"))]
+    (apply dom/ul (clj->js attrs) children)))
+
+(defn inline-ul
+  "Render an inline unordered list"
+  [attrs & children]
+  (let [attrs (update attrs :className #(str " list-inline"))]
+    (apply dom/ul (clj->js attrs) children)))
+
+(def table-styles #{:striped :bordered :condensed :hover})
+
+(defn table
+  "Renders a table. attrs can contain any normal react attributes, including :className.
+
+  Extended attributes that you can include are:
+
+  styles - A set of one or more of: #{:striped :bordered :condensed :hover}
+  "
+  [{:keys [className styles] :as attrs} & children]
+  (let [style-classes (str/join " " (map #(str "table-" (name %)) (set/intersection table-styles styles)))
+        classes       (str (when className (str className " ")) "table " style-classes)]
+    (apply dom/table (clj->js (assoc attrs :className classes)) children)))
+
+(defn labeled-input
+  "An input with a label. All of the attrs apply to the input itself. You must supply a type and id for the
+  field to work correctly. DO NOT USE for checkbox or radio.
+
+  The additional attributes are supported:
+
+  :split - A number from 1 to 11. The number of `sm` columns to allocate to the field label. If not specified, the
+  label will be above the input. The parent must use the `.form-horizontal` class.
+  :help - A string. If supplied, displayed as the help text for the field. NOT shown if warning, error, or success are set.
+
+  You should only ever supply zero or one of the following:
+  :warning - A boolean or string. If set as a string, that content replaces the help text.
+  :error - A boolean or string. If set as a string, that content replaces the help text.
+  :success - A boolean or string. If set as a string, that content replaces the help text.
+  "
+  [{:keys [id type className placeholder split help warning error success] :as attrs} label]
+  (let [state-class        (cond
+                             error " has-error"
+                             warning " has-warning"
+                             success " has-success"
+                             :else "")
+        form-group-classes (str "form-group" state-class)
+        help               (first (keep identity [error warning success help]))
+        split-right        (- 12 split)
+        help-id            (str id "-help")
+        attrs              (cond-> (dissoc attrs :split :help :warning :error :success)
+                             help (assoc :aria-describedby help-id)
+                             :always (update :className #(str " form-control")))]
+    (cond
+      (int? split) (dom/div #js {:className form-group-classes}
+                     (dom/label #js {:className (str "control-label col-sm-" split) :htmlFor id} label)
+                     (dom/div #js {:className (str "col-sm-" split-right)}
+                       (dom/input (clj->js attrs))
+                       (when help (dom/span #js {:id help-id :className "help-block"} help))))
+      :else (dom/div #js {:className form-group-classes}
+              (dom/label #js {:className "control-label" :htmlFor id} label)
+              (dom/input (clj->js attrs))
+              (when help (dom/span #js {:id help-id :className "help-block"} help))))))
