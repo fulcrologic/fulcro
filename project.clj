0090fead269a317e609a7d85f675e6b9099b9181
Tony Kay <tony.kay@gmail.com>
2017-07-12 11:20:44 -0700

    updated version ref

--- project.clj
+++ project.clj
@@ -16,7 +16,7 @@
                  [commons-codec "1.10"]
                  [com.stuartsierra/component "0.3.2"]
                  [com.taoensso/timbre "4.10.0"]
-                 [fulcrologic/fulcro-spec "1.0.0-beta1" :scope "test" :exclusions [fulcrologic/fulcro]]
+                 [fulcrologic/fulcro-spec "1.0.0-beta2-SNAPSHOT" :scope "test" :exclusions [fulcrologic/fulcro]]
                  [org.clojure/core.async "0.3.443" :exclusions [org.clojure/tools.reader]]
                  [com.ibm.icu/icu4j "58.2"]                 ; needed for i18n on server-side rendering
                  [bidi "2.1.1"]
