5f6b5a41335e000a1afb1f25daba1c933f2630b2
Tony Kay <tony.kay@gmail.com>
2017-07-15 21:54:07 -0700

    next beta

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject fulcrologic/fulcro "1.0.0-beta4"
+(defproject fulcrologic/fulcro "1.0.0-beta5-SNAPSHOT"
   :description "A library for building full-stack SPA webapps in Clojure and Clojurescript"
   :url ""
   :license {:name "MIT"
