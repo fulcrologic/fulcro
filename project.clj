2a1c4678e2446436434552b9340b77f728af0bd3
Tony Kay <tony.kay@gmail.com>
2017-11-17 09:44:10 -0800

    fixed project file mis-merge
    Took 6 minutes

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject fulcrologic/fulcro "2.0.0"
+(defproject fulcrologic/fulcro "2.0.0-SNAPSHOT"
   :description "A library for building full-stack SPA webapps in Clojure and Clojurescript"
   :url ""
   :license {:name "MIT"
