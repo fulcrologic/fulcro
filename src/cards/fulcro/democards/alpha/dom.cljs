558f9ab460563f35c540919e01ecd80117bcf371
Tony Kay <tony.kay@gmail.com>
2018-03-01 18:58:10 -0800

    fixed refs on wrapped inputs
    Took 2 hours 28 minutes

--- src/cards/fulcro/democards/alpha/dom.cljs
+++ src/cards/fulcro/democards/alpha/dom.cljs
@@ -2,6 +2,7 @@
   (:require [devcards.core :as dc]
             [fulcro.client :as fc]
             [fulcro.client.cards :refer [defcard-fulcro]]
+            [fulcro.client.dom :as old-dom]
             [fulcro.client.alpha.dom :as dom]
             [goog.object :as gobj]
             [fulcro.client.primitives :as prim :refer [defui defsc InitialAppState initial-state]]
@@ -35,3 +36,57 @@
 (defcard-fulcro attr-symbolic-enumeration
   "Part or all of these attrs are symbolic and resolved at runtime."
   AttrSymbolic)
+
+(defsc Form [this {:keys [:db/id :form/value] :as props}]
+  {:query             [:db/id :form/value]
+   :ident             [:form/by-id :db/id]
+   :initial-state     {:db/id 1 :form/value 22}
+   :componentDidMount (fn [] (when-let [e (gobj/get this "n")] (.focus e)))}
+  (old-dom/input #js {:onChange #(m/set-string! this :form/value :event %)
+                      :ref      (fn [r] (gobj/set this "n" r))
+                      :value    value}))
+
+(defsc OldForm [this {:keys [:db/id :form/value] :as props}]
+  {:query             [:db/id :form/value]
+   :ident             [:form/by-id :db/id]
+   :initial-state     {:db/id 1 :form/value 22}
+   :componentDidMount (fn [] (when-let [e (dom/node this "thing")] (.focus e)))}
+  (old-dom/input #js {:onChange #(m/set-string! this :form/value :event %)
+                      :ref      "thing"
+                      :value    value}))
+
+(def ui-form (prim/factory Form {:keyfn :db/id}))
+(def ui-old-form (prim/factory OldForm {:keyfn :db/id}))
+
+(defsc WrappedInputRoot [this {:keys [db/id form old-form]}]
+  {:query         [:db/id
+                   {:form (prim/get-query Form)}
+                   {:old-form (prim/get-query OldForm)}]
+   :ident         [:wrapped-input-root/by-id :db/id]
+   :initial-state {:db/id 1 :form {}}}
+  (dom/div nil
+    (ui-form form)))
+
+(defsc OldWrappedInputRoot [this {:keys [db/id form]}]
+  {:query         [:db/id
+                   {:form (prim/get-query OldForm)}]
+   :ident         [:wrapped-input-root/by-id :db/id]
+   :initial-state {:db/id 1 :form {}}}
+  (dom/div nil
+    (ui-old-form form)))
+
+(defcard-fulcro wrapped-input-card-string-refs
+  "# Inputs with (Deprecated) String ref.
+
+  Click on the card to show only this card and reload the page (it should auto-focus on mount)"
+  OldWrappedInputRoot
+  {}
+  {:inspect-data true})
+
+(defcard-fulcro wrapped-input-card-fn-refs
+  "# Input with fn ref
+
+  Click on the card to show only this card and reload the page (it should auto-focus on mount)"
+  WrappedInputRoot
+  {}
+  {:inspect-data true})
--- src/main/fulcro/client/alpha/dom.cljs
+++ src/main/fulcro/client/alpha/dom.cljs
@@ -6,58 +6,6 @@
             [fulcro.client.util :as util]
             [goog.object :as gobj]))
 
-(defn- update-state
-  "Updates the state of the wrapped input element."
-  [component next-props value]
-  (let [on-change  (gobj/getValueByKeys component "state" "onChange")
-        next-state #js {}]
-    (gobj/extend next-state next-props #js {:onChange on-change})
-    (gobj/set next-state "value" value)
-    (.setState component next-state)))
-
-(defn wrap-form-element [element]
-  (let [ctor (fn [props]
-               (this-as this
-                 (set! (.-state this)
-                   (let [state #js {}]
-                     (->> #js {:onChange (goog/bind (gobj/get this "onChange") this)}
-                       (gobj/extend state props))
-                     state))
-                 (.apply js/React.Component this (js-arguments))))]
-    (set! (.-displayName ctor) (str "wrapped-" element))
-    (goog.inherits ctor js/React.Component)
-    (specify! (.-prototype ctor)
-      Object
-      (onChange [this event]
-        (when-let [handler (.-onChange (.-props this))]
-          (handler event)
-          (update-state
-            this (.-props this)
-            (gobj/getValueByKeys event "target" "value"))))
-
-      (componentWillReceiveProps [this new-props]
-        (let [state-value   (gobj/getValueByKeys this "state" "value")
-              element-value (gobj/get (js/ReactDOM.findDOMNode this) "value")]
-          ;; On IE, onChange event might come after actual value of
-          ;; an element have changed. We detect this and render
-          ;; element as-is, hoping that next onChange will
-          ;; eventually come and bring our modifications anyways.
-          ;; Ignoring this causes skipped letters in controlled
-          ;; components
-          ;; https://github.com/facebook/react/issues/7027
-          ;; https://github.com/reagent-project/reagent/issues/253
-          ;; https://github.com/tonsky/rum/issues/86
-          ;; TODO: Find a better solution, since this conflicts
-          ;; with controlled/uncontrolled inputs.
-          ;; https://github.com/r0man/sablono/issues/148
-          (if (not= state-value element-value)
-            (update-state this new-props element-value)
-            (update-state this new-props (gobj/get new-props "value")))))
-
-      (render [this]
-        (js/React.createElement element (.-state this))))
-    (js/React.createFactory ctor)))
-
 ;; (dom/gen-react-dom-fns)
 
 (defn render
