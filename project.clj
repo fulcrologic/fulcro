56aeca188a512d3fbf1a23c5b636f205ba2dd4a5
Tony Kay <tony.kay@gmail.com>
2018-03-06 12:11:11 -0800

    version bump

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject fulcrologic/fulcro "2.3.1"
+(defproject fulcrologic/fulcro "2.3.2-SNAPSHOT"
   :description "A library for building full-stack SPA webapps in Clojure and Clojurescript"
   :url ""
   :license {:name "MIT"
