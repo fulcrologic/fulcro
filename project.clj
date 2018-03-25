d9f1afd7379522c120455012051dba16ce417fa3
Tony Kay <tony.kay@gmail.com>
2018-03-25 13:18:26 -0700

    2.5 alpha1
    Took 53 seconds

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject fulcrologic/fulcro "2.5.0-SNAPSHOT"
+(defproject fulcrologic/fulcro "2.5.0-alpha1"
   :description "A library for building full-stack SPA webapps in Clojure and Clojurescript"
   :url ""
   :license {:name "MIT"
