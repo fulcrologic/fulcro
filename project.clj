5c7970c4c27e4ac1c7c94d09557e760957658389
Tony Kay <tony.kay@gmail.com>
2018-06-01 06:16:47 -0700

    added missing react requires

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject fulcrologic/fulcro "2.5.6"
+(defproject fulcrologic/fulcro "2.5.7-SNAPSHOT"
   :description "A library for building full-stack SPA webapps in Clojure and Clojurescript"
   :url ""
   :lein-min-version "2.8.1"
