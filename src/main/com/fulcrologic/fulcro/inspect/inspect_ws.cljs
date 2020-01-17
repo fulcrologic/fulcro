01ba4349bf27dbd94cbdee692be63c5cd2710f53
Anthony D'Ambrosio <anthony.dayzerostudio@gmail.com>
2020-01-17 14:48:32 -0800

    checkpoint: working

--- src/main/com/fulcrologic/fulcro/inspect/inspect_ws.cljs
+++ src/main/com/fulcrologic/fulcro/inspect/inspect_ws.cljs
@@ -4,6 +4,7 @@
     [goog.object :as gobj]
     [clojure.pprint :refer [pprint]]
     [com.fulcrologic.fulcro.inspect.inspect-client :as inspect]
+    [com.fulcrologic.fulcro.inspect.transit :as inspect.transit]
     [taoensso.timbre :as log]
     [taoensso.sente :as sente]
     [com.fulcrologic.fulcro.networking.transit-packer :as tp]))
@@ -16,22 +17,29 @@
 (defn start-ws-messaging! []
   (when-not @sente-socket-client
     (reset! sente-socket-client
-      (let [{:keys [ch-recv state send-fn] :as result} (sente/make-channel-socket-client! "/chsk" "no-token-desired"
-                                                         {:type   :auto
-                                                          :host   (str SERVER_HOST ":" SERVER_PORT)
-                                                          :packer (tp/make-packer {})})]
+      (let [{:keys [ch-recv state send-fn] :as result}
+            (sente/make-channel-socket-client! "/chsk" "no-token-desired"
+              {:type   :auto
+               :host   (str SERVER_HOST ":" SERVER_PORT)
+               :packer (tp/make-packer {:read inspect.transit/read-handlers
+                                        :write inspect.transit/write-handlers})})]
         (log/debug "Starting websockets")
         (go-loop []
-          (when-let [[type data :as msg] (<! inspect/send-ch)]
-            (log/info "Inspect wants to send" (with-out-str (pprint msg)))
-            (send-fn [:inspect/message {:type type :data data :timestamp (js/Date.)}] 10000 (fn [resp] (log/info "Inspect responded with" (with-out-str (pprint resp)))))
-            (recur)))
+          (if (-> state deref :open?)
+            (when-let [[type data :as msg] (<! inspect/send-ch)]
+              (log/info "Inspect wants to send" (with-out-str (pprint msg)))
+              (send-fn [:inspect/message {:type type :data data :timestamp (js/Date.)}] 10000))
+            (do
+              (log/info "Send attempted before channel ready...waiting")
+              (async/<! (async/timeout 1000))))
+          (recur))
         (go-loop []
           (if (-> state deref :open?)
-            (when-some [msg (<! ch-recv)]
-              (log/info "Incoming message from electron:" (with-out-str (pprint msg)))
-              ; Send message to inspect's handler on this side...i.e. get current app state
-              (inspect/handle-devtool-message msg))
+            (when-some [{:keys [event]} (<! ch-recv)]
+              (log/debug "Incoming event from electron:" (with-out-str (pprint event)))
+              (let [[_ [_ msg]] event]
+                (log/info "Incoming message from electron:" (with-out-str (pprint msg)))
+                (inspect/handle-devtool-message msg)))
             (do
               (log/info "Send attempted before channel ready...waiting")
               (async/<! (async/timeout 1000))))
