d5b1281e07532248c8d815f7a9a1a46612465a1e
Anthony D'Ambrosio <anthony.dayzerostudio@gmail.com>
2020-01-17 18:43:48 -0800

    cleanup & adding backoff to sente and go loops

--- src/main/com/fulcrologic/fulcro/inspect/inspect_ws.cljs
+++ src/main/com/fulcrologic/fulcro/inspect/inspect_ws.cljs
@@ -1,50 +1,58 @@
 (ns ^:no-doc com.fulcrologic.fulcro.inspect.inspect-ws
   (:require
     [cljs.core.async :as async :refer [>! <!] :refer-macros [go go-loop]]
-    [goog.object :as gobj]
     [clojure.pprint :refer [pprint]]
     [com.fulcrologic.fulcro.inspect.inspect-client :as inspect]
     [com.fulcrologic.fulcro.inspect.transit :as inspect.transit]
-    [taoensso.timbre :as log]
+    [com.fulcrologic.fulcro.networking.transit-packer :as tp]
+    [taoensso.encore :as enc]
     [taoensso.sente :as sente]
-    [com.fulcrologic.fulcro.networking.transit-packer :as tp]))
+    [taoensso.timbre :as log]))
 
 (goog-define SERVER_PORT "8237")
 (goog-define SERVER_HOST "localhost")
 
 (defonce sente-socket-client (atom nil))
 
+(def backoff-ms #(enc/exp-backoff % {:max 15000}))
+
 (defn start-ws-messaging! []
   (when-not @sente-socket-client
     (reset! sente-socket-client
-      (let [{:keys [ch-recv state send-fn] :as result}
-            (sente/make-channel-socket-client! "/chsk" "no-token-desired"
-              {:type   :auto
-               :host   (str SERVER_HOST ":" SERVER_PORT)
-               :packer (tp/make-packer {:read inspect.transit/read-handlers
-                                        :write inspect.transit/write-handlers})})]
-        (log/debug "Starting websockets")
-        (go-loop []
-          (if (-> state deref :open?)
-            (when-let [[type data :as msg] (<! inspect/send-ch)]
-              (log/info "Inspect wants to send" (with-out-str (pprint msg)))
-              (send-fn [:inspect/message {:type type :data data :timestamp (js/Date.)}] 10000))
+      (sente/make-channel-socket-client! "/chsk" "no-token-desired"
+        {:type           :auto
+         :host           (str SERVER_HOST ":" SERVER_PORT)
+         :packer         (tp/make-packer {:read  inspect.transit/read-handlers
+                                          :write inspect.transit/write-handlers})
+         :wrap-recv-evs? false
+         :backoff-ms-fn  backoff-ms}))
+    (log/debug "Starting websockets")
+    (let [{:keys [state send-fn]} @sente-socket-client]
+      (go-loop [attempt 1]
+        (let [open? (:open? @state)]
+          (if open?
+            (when-let [[type data] (<! inspect/send-ch)]
+              (log/debug "Forwarding to server: type =" type)
+              (log/trace "Forwarding to server: data =" data)
+              (send-fn [:fulcro.inspect/message {:type type :data data :timestamp (js/Date.)}]))
             (do
-              (log/info "Send attempted before channel ready...waiting")
-              (async/<! (async/timeout 1000))))
-          (recur))
-        (go-loop []
-          (if (-> state deref :open?)
-            (when-some [{:keys [event]} (<! ch-recv)]
-              (log/debug "Incoming event from electron:" (with-out-str (pprint event)))
-              (let [[_ [_ msg]] event]
-                (log/info "Incoming message from electron:" (with-out-str (pprint msg)))
-                (inspect/handle-devtool-message msg)))
+              (log/trace (str "Waiting for channel to be ready"))
+              (async/<! (async/timeout (backoff-ms attempt)))))
+          (recur (if open? 1 (inc attempt))))))
+    (let [{:keys [state ch-recv]} @sente-socket-client]
+      (go-loop [attempt 1]
+        (let [open? (:open? @state)]
+          (if open?
+            (do (enc/when-let [[event-type message] (:event (<! ch-recv))
+                               _ (= :fulcro.inspect/event event-type)
+                               {:as msg :keys [type data]} message]
+                  (log/debug "Forwarding from electron: type =" type)
+                  (log/trace "Forwarding from electron: data =" data)
+                  (inspect/handle-devtool-message msg)))
             (do
-              (log/info "Send attempted before channel ready...waiting")
-              (async/<! (async/timeout 1000))))
-          (recur))
-        result))))
+              (log/trace (str "Waiting for channel to be ready"))
+              (async/<! (async/timeout (backoff-ms attempt)))))
+          (recur (if open? 1 (inc attempt))))))))
 
 (defn install-ws []
   (when-not @inspect/started?*
