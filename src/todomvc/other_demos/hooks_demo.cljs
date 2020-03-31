a5fa762b468d3bffe91a815c2188cb1cb8b883f8
Tony Kay <tony.kay@gmail.com>
2020-03-31 09:44:11 -0700

    renamed demos

--- src/todomvc/other_demos/hooks_demo.cljs
+++ src/todomvc/other_demos/hooks_demo.cljs
@@ -1,4 +1,4 @@
-(ns roots.hooks-demo
+(ns other-demos.hooks-demo
   (:require
     [com.fulcrologic.fulcro.rendering.multiple-roots-renderer :as mroot]
     [com.fulcrologic.fulcro.algorithms.indexing :as index]
@@ -44,38 +44,67 @@
                             (str x))))}
       (fn [] Hook)))
 
-(defsc SomeHookChild [this {:child/keys [id label] :as props}]
-  {:query         [:child/id :child/label]
-   :initial-state {:child/id :param/id :child/label (str "some child")}
-   :ident         :child/id}
-  (dom/div "Child " label
+(defsc SomeHookChild [this {:child/keys [id label other] :as props}]
+  {:query                [:child/id :child/label
+                          {:child/other (comp/get-query OtherChild)}]
+   :initial-state        {:child/id    :param/id
+                          :child/label (str "some child")
+                          :child/other {:id 42 :n 1000}}
+   :componentWillUnmount (fn [this] (log/info "SHC UNMOUNT"))
+   :ident                :child/id}
+  (dom/div
+    (dom/div "Child " label)
+    (ui-other-child other)
     (dom/input {:value   label
                 :onInput (fn [evt] (m/set-string! this :child/label :event evt))})))
 
 (def ui-some-hook-child (comp/factory SomeHookChild {:keyfn :child/id}))
 
+;; Without defsc
+#_(def Hook
+    (comp/configure-hooks-component!
+      (fn [this {:hook/keys [id x child] :as props}]
+        (let [[v set-v!] (comp/use-state 0)]
+          (dom/div "This is a hooks-based component: "
+            (ui-some-hook-child child)
+            (dom/button {:onClick #(set-v! (inc v))} (str v))
+            (dom/button {:onClick #(m/set-integer! this :hook/x :value (inc x))}
+              (str x)))))
+      {:query         (fn [_] [:hook/id :hook/x {:hook/child (comp/get-query SomeHookChild)}])
+       :ident         (fn [_ {:hook/keys [id]}] [:hook/id id])
+       :componentName ::Hook
+       :initial-state (fn [{:keys [id]}]
+                        {:hook/x     1
+                         :hook/id    id
+                         :hook/child (comp/get-initial-state SomeHookChild {:id id})})}))
+
+;; with defsc
 (defsc Hook [this {:hook/keys [id x child] :as props}]
   {:query         [:hook/id :hook/x {:hook/child (comp/get-query SomeHookChild)}]
    :ident         :hook/id
-   :initial-state {:hook/x     1 :hook/id :param/id
-                   :hook/child {:id :param/id}}
-   :use-hooks?    true}
-  (let [[v set-v!] (comp/use-state 0)]
+   :initial-state (fn [{:keys [id child-id]}]
+                    {:hook/x     1
+                     :hook/id    id
+                     :hook/child (comp/get-initial-state SomeHookChild {:id child-id})})
+   #_#_:use-hooks? true}
+  (let [#_#_[v set-v!] (comp/use-state 0)]
     (dom/div "This is a hooks-based component: "
-      (js/console.log comp/*depth* comp/*shared* comp/*parent*)
       (ui-some-hook-child child)
-      (dom/button {:onClick #(set-v! (inc v))} (str v))
+      ;(dom/button {:onClick #(set-v! (inc v))} (str v))
       (dom/button {:onClick #(m/set-integer! this :hook/x :value (inc x))}
         (str x)))))
 
 (def ui-hook (comp/factory Hook {:keyfn :hook/id}))
 
-(defsc Root [this {:keys [hook hooks] :as props}]
+(defsc Root [this {:keys [hook hooks normal-child] :as props}]
   {:query         [{:hook (comp/get-query Hook)}
-                   {:hooks (comp/get-query Hook)}]
-   :initial-state {:hook  {:id 1}
-                   :hooks [{:id 2} {:id 3}]}}
+                   {:hooks (comp/get-query Hook)}
+                   {:normal-child (comp/get-query OtherChild)}]
+   :initial-state {:hook         {:id 1 :child-id 100}
+                   :hooks        [{:id 2 :child-id 101} {:id 3 :child-id 102}]
+                   :normal-child {:id 1000 :n 100}}}
   (dom/div
+    (ui-other-child normal-child)
     (ui-hook hook)
     (dom/h2 "Children")
     (map ui-hook hooks)))
