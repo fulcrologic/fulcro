ca86b9aac3eb386f569e6551fb6e7489d1cb0e77
Björn Ebbinghaus <bjoern@ebbinghaus.me>
2021-11-08 12:27:06 +0100

    Fix and improve `>defn` clj-kondo hook

--- src/clj-kondo/clj-kondo.exports/com.fulcrologic/fulcro/com/fulcrologic/fulcro/clj_kondo_hooks.clj
+++ src/clj-kondo/clj-kondo.exports/com.fulcrologic/fulcro/com/fulcrologic/fulcro/clj_kondo_hooks.clj
@@ -35,6 +35,9 @@
                               :type    :clj-kondo.fulcro.defmutation/handler-arity})))))
     {:node new-node}))
 
+(def =>? #{'=> :ret})
+(def |? #{'| :st})
+
 (defn >defn
   [{:keys [node]}]
   (let [args       (rest (:children node))
@@ -54,9 +57,39 @@
                      argv
                      gspec
                      body))]
-    (when (not= (count (api/sexpr argv))
-                (count (take-while #(not= '=> %) (api/sexpr gspec))))
+    ;; gspec: [arg-specs* (| arg-preds+)? => ret-spec (| fn-preds+)? (<- generator-fn)?]
+    (if (not= 1 (count (filter =>? (api/sexpr gspec))))
       (api/reg-finding! (merge (meta gspec)
-                               {:message "Guardrail spec does not match function signature"
-                                :type    :clj-kondo.fulcro.>defn/signature-mismatch})))
+                          {:message (str "Gspec requires exactly one `=>` or `:ret`")
+                           :type    :clj-kondo.fulcro.>defn/invalid-gspec}))
+      (let [p (partition-by (comp not =>? api/sexpr) (:children gspec))
+            [arg [=>] [ret-spec & _output]] (if (-> p ffirst api/sexpr =>?)
+                                              (cons [] p) ; arg-specs might be empty
+                                              p)
+            [arg-specs [| & arg-preds]] (split-with (comp not |? api/sexpr) arg)]
+
+        (when-not ret-spec
+          (println =>)
+          (api/reg-finding! (merge (meta =>)
+                              {:message "Missing return spec."
+                               :type    :clj-kondo.fulcro.>defn/invalid-gspec})))
+
+        ;; (| arg-preds+)?
+        (when (and | (empty? arg-preds))
+          (api/reg-finding! (merge (meta |)
+                              {:message "Missing argument predicates after |."
+                               :type    :clj-kondo.fulcro.>defn/invalid-gspec})))
+
+
+        (let [len-argv (count (remove #{'&} (api/sexpr argv))) ; [a & more] => 2 arguments
+              arg-difference (- (count arg-specs) len-argv)]
+          (when (not (zero? arg-difference))
+            (let [too-many-specs? (pos? arg-difference)]
+              (api/reg-finding! (merge
+                                  (meta (if too-many-specs?
+                                          (nth arg-specs (+ len-argv arg-difference -1)) ; first excess spec
+                                          gspec)) ; The gspec is wrong, not the surplus argument.
+                                  {:message (str "Guardrail spec does not match function signature. "
+                                              "Too " (if too-many-specs? "many" "few") " specs.")
+                                   :type    :clj-kondo.fulcro.>defn/invalid-gspec})))))))
     {:node new-node}))
