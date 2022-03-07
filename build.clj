71af806e4fe7748f1d8ede5839636006cd50ade7
Jakub Holý <jakubholy@jakubholy.net>
2022-03-07 23:28:27 +0100

    Build the jar using Tools Build (#504)
    * Build the jar using Tools Build so that we can build the jar on the CI / GH Action.
    * use deps.edn :paths -> incl. kondo
    * Add running Check CljDoc to GH PRs

---
+++ build.clj
@@ -0,0 +1,24 @@
+(ns build
+  "Fulcro's build script
+    
+  clojure -A:build -T:build jar
+    
+  For more information, run:
+
+  clojure -A:deps -T:build help/doc"
+  (:require 
+    [clojure.edn :as edn]
+    [org.corfield.build :as bb]))
+
+(def src-dirs (:paths (edn/read-string (slurp "deps.edn"))))
+
+(def lib 'com.fulcrologic/fulcro)
+(def version "snapshot")
+
+(defn jar "Build the JAR." [opts]
+  (-> opts
+      (bb/clean)
+      (assoc :lib lib :version version 
+        :src-dirs src-dirs
+        :resource-dirs [])
+      (bb/jar)))
