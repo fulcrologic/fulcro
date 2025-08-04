89001a5b0c48e3c573ca79402c14885f1c5ee48e
Tony Kay <tony.kay@gmail.com>
2025-08-04 16:14:13 -0700

    wrapped input fixes

--- src/todomvc/fulcro_todomvc/main.cljs
+++ src/todomvc/fulcro_todomvc/main.cljs
@@ -1,18 +1,20 @@
 (ns fulcro-todomvc.main
   (:require
-    [fulcro-todomvc.ui :as ui]
-    [com.fulcrologic.fulcro.networking.websockets :as fws]
+    [com.fulcrologic.devtools.common.target :refer [ido]]
     [com.fulcrologic.fulcro.algorithms.timbre-support :refer [console-appender prefix-output-fn]]
     [com.fulcrologic.fulcro.application :as app]
     [com.fulcrologic.fulcro.data-fetch :as df]
-    [com.fulcrologic.devtools.common.target :refer [ido]]
-    [fulcro.inspect.tool :as it]
-    [taoensso.timbre :as log]
+    [com.fulcrologic.fulcro.networking.websockets :as fws]
+    [fulcro-todomvc.app :refer [app]]
     [fulcro-todomvc.custom-types :as custom-types]
-    [fulcro-todomvc.app :refer [app]]))
+    [fulcro-todomvc.ui :as ui]
+    [fulcro.inspect.tool :as it]
+    [taoensso.timbre :as log]))
 
 ;; Have to be installed before we create websockets
 (defonce prevent-again (custom-types/install!))
+
+(defn refresh! [] (app/mount! app ui/Root "app"))
 
 (defn ^:export start []
   (app/mount! app ui/Root "app")
